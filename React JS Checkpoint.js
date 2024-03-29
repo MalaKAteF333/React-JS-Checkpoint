ddaaf59f0cb210998ebe5372b14c78725b79e6aa
MalaKAteF333 <158581050+MalaKAteF333@users.noreply.github.com>
2024-03-30 00:36:14 +0200

    Add files via upload

---
+++ React JS Checkpoint.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar,Container, Nav, Card }
+from 'react-bootstrap';
+
+
+function App() {
+    return (
+        <>
+        <div className="App">
+            <Navbar >bg="dark variant= "dark"</Navbar>
+                <countainer>
+                    <Navbar.Brand
+                    herf="#">Navbar</Navbar.Brand>
+                </countainer>
+        </div>
+        </>
+    )
+}
+
+export default App;
